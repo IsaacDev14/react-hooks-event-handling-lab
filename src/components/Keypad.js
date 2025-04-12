995ada32663abbc52bba8ba5cf0c14fe9aa1ff0f
IsaacDev14 <mwitiisaac14@gmail.com>
2025-04-12 05:41:06 +0300

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChnage(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChnage}/>
+        </div>
     )
 }
 
